6fd3ad5a2ed40100e5f4264b3ea5ae0d61baa3b3
Elivis Lives <michael@dfwqualityappraisal.com>
2019-03-18 22:10:48 -0500

    got computer guess working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,32 @@
 /* Declaring letters and other variables */
 
-var computerLetter = ["a",
+var computerLetter = [
+  "a",
   "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z"
+  "c"
+  // "d",
+  // "e",
+  // "f",
+  // "g",
+  // "h",
+  // "i",
+  // "j",
+  // "k",
+  // "l",
+  // "m",
+  // "n",
+  // "o",
+  // "p",
+  // "q",
+  // "r",
+  // "s",
+  // "t",
+  // "u",
+  // "v",
+  // "w",
+  // "x",
+  // "y",
+  // "z"
 ];
 
 var wins = 0,
@@ -33,46 +34,44 @@ var wins = 0,
   guessesNotUsed = 10,
   guessThus = [],
   divYourSelection = document.getElementById("user-guess"),
-  divComputerGuess = document.getElementById("col-compGuess"),
+  divComputerGuess = document.getElementById("computer-guess"),
   divWins = document.getElementById("col-win"),
   divLosses = document.getElementById("col-loss"),
   divGuessesLeft = document.getElementById("col-guessLeft"),
-  divGuessesSoFar = document.getElementById("col-guessSoFar");
-
+  divGuessesSoFar = document.getElementById("col-guessSoFar"),
+  divHeader = document.getElementById("header");
 
 var computerGuess =
-  computerLetter[Math.floor(Math.random() * computerLetter.length)]
+  computerLetter[Math.floor(Math.random() * computerLetter.length)];
 console.log(computerGuess);
 
-document.onkeyup = function (event) {
-
+document.onkeyup = function(event) {
   // Determines which key was pressed by te user.
   var letterGuessed = event.key.toLowerCase();
 
   function userGuessFunction() {
-
-    document.getElementById("user-guessThus").innerHTML = guessThus;
-    console.log(guessThus + "   " + letterGuessed)
+    document.getElementById("col-guessSoFar").innerHTML = guessThus;
+    // console.log(guessThus + "   " + letterGuessed);
   }
   guessThus.push(letterGuessed);
-  userGuessFunction(divYourSelection.textContent = letterGuessed);
+  userGuessFunction((divYourSelection.textContent = letterGuessed));
   // if (userGuessFunction)
 
-  if (computerGuess = letterGuessed) {
-    wins++
-  } else {
-    losses++
-  }
-
-
-
-
+  // if (computerGuess = letterGuessed) {
+  //   wins++
+  // } else {
+  //   losses++
+  // }
 
   /* Write to HTML */
   divYourSelection.textContent = letterGuessed;
   divWins.textContent = wins;
-  divLosses.textContent = losses;
+  divComputerGuess.textContent = computerGuess;
+
+  // divLosses.textContent = losses;
+  // divHeader.addEventListener("click", function () {
+  //   console.log("I changed the Header");
+
   // divGuessesSoFar.push = user-guessThus;
   // divGuessesLeft.textContent = guessesNotUsed;        //   divWins.textContent = col;
-  // divComputerGuess.textContent= computer-guess;
-}
+};
