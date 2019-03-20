ec1f4ce7dc77d2e433ea305a007164f8144c91e1
dallasappraiser <46821219+dallasappraiser@users.noreply.github.com>
2019-03-20 17:36:29 -0500

    Add files via upload
    removing duplicates, need to work on count

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,43 +3,46 @@
 var computerLetter = [
   "a",
   "b",
-  "c"
-  // "d",
-  // "e",
-  // "f",
-  // "g",
-  // "h",
-  // "i",
-  // "j",
-  // "k",
-  // "l",
-  // "m",
-  // "n",
-  // "o",
-  // "p",
-  // "q",
-  // "r",
-  // "s",
-  // "t",
-  // "u",
-  // "v",
-  // "w",
-  // "x",
-  // "y",
-  // "z"
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
 ];
 
 var wins = 0,
   losses = 0,
   guessesNotUsed = 10,
   guessThus = [],
+  noDuplicates = [],
   divYourSelection = document.getElementById("user-guess"),
   divComputerGuess = document.getElementById("computer-guess"),
   divWins = document.getElementById("col-win"),
   divLosses = document.getElementById("col-loss"),
   divGuessesLeft = document.getElementById("col-guessLeft"),
   divGuessesSoFar = document.getElementById("col-guessSoFar"),
-  divHeader = document.getElementById("header");
+  wrongKey = document.getElementById("wrong-key"),
+  youWinAlert = document.getElementById("you-win"),
+  newLetter = document.getElementById("new-letter");
 
 var computerGuess =
   computerLetter[Math.floor(Math.random() * computerLetter.length)];
@@ -49,29 +52,75 @@ document.onkeyup = function(event) {
   // Determines which key was pressed by te user.
   var letterGuessed = event.key.toLowerCase();
 
+  // user Guess Function
   function userGuessFunction() {
-    document.getElementById("col-guessSoFar").innerHTML = guessThus;
-    // console.log(guessThus + "   " + letterGuessed);
+    guessThus.push(letterGuessed);
   }
-  guessThus.push(letterGuessed);
-  userGuessFunction((divYourSelection.textContent = letterGuessed));
-  // if (userGuessFunction)
 
-  // if (computerGuess = letterGuessed) {
-  //   wins++
+  // Remove duplicates from guesses
+  var noDuplicates = [];
+  dojo.forEach(guessThus, function(item, i) {
+    if (dojo.indexOf(noDuplicates, item) == -1) {
+      noDuplicates.push(item);
+    }
+  });
+  document.getElementById("col-guessSoFar").textContent = noDuplicates;
+  console.log(console.log(noDuplicates));
+
+  // Track Wins
+
+  if (letterGuessed == computerGuess) {
+    wins++;
+    youWinAlert.style.visibility = "visible";
+    guessThus = [];
+    guessesNotUsed = 10;
+  } else {
+    youWinAlert.style.visibility = "hidden";
+  }
+
+  // Track losses
+
+  if (letterGuessed !== computerGuess && guessesNotUsed >= 0) {
+    guessesNotUsed--;
+  }
+
+  // Deduct guessesNotUsed  with wrong answers
+  if (guessesNotUsed == 0) {
+    losses++;
+    guessThus = [];
+    guessesNotUsed = 10;
+  }
+
+  // Only allow letters to be guessed
+
+  if (computerLetter.indexOf(letterGuessed) == -1) {
+    // alert("Please select the letters a - z only!");
+    // wrongKey.style.display = "normal";
+    wrongKey.style.visibility = "visible";
+  } else {
+    wrongKey.style.visibility = "hidden";
+  }
+
+  // do not allow for repetitive keystrokes
+
+  // if (guessThus.indexOf(letterGuessed) >= -1) {
+  //   newLetter.style.visibility = "visible";
   // } else {
-  //   losses++
+  //   newLetter.style.visibility = "hidden";
+  // }
+
+  // function lettersOnly(letterGuessed) {
+  //   var lettersOnly = /[^a-z^]/gi;
   // }
 
   /* Write to HTML */
   divYourSelection.textContent = letterGuessed;
-  divWins.textContent = wins;
   divComputerGuess.textContent = computerGuess;
-
-  // divLosses.textContent = losses;
-  // divHeader.addEventListener("click", function () {
-  //   console.log("I changed the Header");
-
+  divWins.textContent = wins;
+  divLosses.textContent = losses;
+  divGuessesLeft.textContent = guessesNotUsed;
+  userGuessFunction((divYourSelection.textContent = letterGuessed));
   // divGuessesSoFar.push = user-guessThus;
-  // divGuessesLeft.textContent = guessesNotUsed;        //   divWins.textContent = col;
+  //   divWins.textContent = col;
+  // divComputerGuess.textContent= computer-guess;
 };
